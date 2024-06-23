e9c923105728a41390bfec5b89173e94b2aea60d
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-06-23 23:34:28 +0700

    feat: edit-delete-category-feature

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -66,15 +67,32 @@ class CategoryController extends Controller
     public function edit(Category $category)
     {
         //
-        return view('admin.edit.index', compact('category'));
+        return view('admin.categories.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
+    public function update(UpdateCategoryRequest $request, Category $category)
     {
         //
+        DB::transaction(function () use ($request, $category) {
+            $validated = $request->validate([
+                'name' => 'required|string|max:255',
+                'icon' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+            ]);
+
+            if ($request->hasFile('icon')) {
+                $iconPath = $request->file('icon')->store('icons', 'public');
+                $validated['icon'] = $iconPath;
+            }
+
+            $validated['slug'] = Str::slug($validated['name']);
+
+            $category->update($validated);
+        });
+
+        return redirect()->route('admin.categories.index')->with('success', 'Category updated successfully');
     }
 
     /**
@@ -83,5 +101,18 @@ class CategoryController extends Controller
     public function destroy(Category $category)
     {
         //
+        DB::beginTransaction();
+
+        try {
+            $category->delete();
+
+            DB::commit();
+
+            return redirect()->route('admin.categories.index')->with('success', 'Category deleted successfully');
+        } catch (\Exception $e) {
+            //throw $th;
+            DB::rollBack();
+            return redirect()->route('admin.categories.index')->with('error', 'Category failed to delete');
+        }
     }
 }
