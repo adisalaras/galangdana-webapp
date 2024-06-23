d696d3e53fd2defc0f3d2455fb6f19e3cfeef0f2
AdrianRamadhan <adrianramadhan881@gmail.com>
2024-06-23 17:09:58 +0700

    feat: add-database-seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $ownerRole = Role::create(['name' => 'owner']);
+
+        $fundraiserRole = Role::create(['name' => 'fundraiser']);
+
+        $userowner = User::create([
+            'name' => 'Adrian Ramadhan',
+            'avatar' => 'images/default-avatar.jpg',
+            'email' => 'adrian@owner.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $userowner->assignRole($ownerRole);
+    }
+}
